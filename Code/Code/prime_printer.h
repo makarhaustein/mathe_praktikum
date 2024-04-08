7db663c630e54890fcceac94801696918429d0d7
Quickblade22 <115694928+Quickblade22@users.noreply.github.com>
2024-04-08 19:32:36 +0200

    Schritt 5

--- Code/Code/prime_printer.h
+++ Code/Code/prime_printer.h
@@ -4,53 +4,55 @@
 #include <string>
 #include <iostream>
 #include <iomanip>
+# include < vector >
+using std :: vector ;
 
 void print() {
-  int max_number_of_primes = 300;
-  int rows = 50;
-  int coloumn = 4;
-  int ordmax = 30;
-  int no_of_primes[max_number_of_primes+1];
+  const int max_number_of_primes = 300;
+  const int rows = 50;
+  const int coloumn = 4;
+  const int ordmax = 30;
+  vector<int> no_of_primes(max_number_of_primes+1);
   int pagenumber;
   int pageoffset;
   int rowoffset;
-  int C;
-  int J;
-  int K;
+  int c;
+  int j;
+  int k;
   bool jprime;
   int ord;
   int SQUARE;
-  int N=0;
-  int MULT[ordmax+1];
+  int N;
+  vector<int> MULT(ordmax+1);
 
-  J=1;
-  K=1;
+  j=1;
+  k=1;
   no_of_primes[1] = 2;
   ord = 2;
   SQUARE = 9;
 
-  while (K < max_number_of_primes) {
+  while (k < max_number_of_primes) {
     do {
-        J += 2;
-        if (J == SQUARE) {
+        j += 2;
+        if (j == SQUARE) {
           ord++;  
           SQUARE=no_of_primes[ord]*no_of_primes[ord];   
-          MULT[ord-1]=J;
+          MULT[ord-1]=j;
         }
         N=2;
         jprime=true;
         while (N < ord && jprime) { 
-          while (MULT[N] < J){
+          while (MULT[N] < j){
             MULT[N] += no_of_primes[N] + no_of_primes[N]; 
           }
-          if (MULT[N] == J) {
+          if (MULT[N] == j) {
             jprime=false;
           } 
           N++;
         }
     } while (!jprime);
-    K++;
-    no_of_primes[K]=J;
+    k++;
+    no_of_primes[k]=j;
   }
 
   pagenumber = 1;
@@ -63,9 +65,9 @@ void print() {
     std::cout << pagenumber;
     std::cout << std::endl;
     for (rowoffset=pageoffset; rowoffset <= pageoffset+rows-1; rowoffset++) {
-      for (C=0; C <= coloumn-1; C++){
-        if (rowoffset+C*rows <= max_number_of_primes){
-          std::cout << std::setw(10) << no_of_primes[rowoffset+C*rows];
+      for (c=0; c <= coloumn-1; c++){
+        if (rowoffset+c*rows <= max_number_of_primes){
+          std::cout << std::setw(10) << no_of_primes[rowoffset+c*rows];
         }
       }
       std::cout << std::endl;
