5208375af9efb34c41b024d33743811243b77736
Makar Haustein <makarmaxha@gmail.com>
2024-05-15 14:28:40 +0200

    Aufgabe 3 Lösung

--- Code (3)/quadratur.cpp
+++ Code (3)/quadratur.cpp
@@ -1,14 +1,43 @@
-#include "unit.h"
 #include "quadratur.h"
+#include "unit.h"
+#include <cmath>
 #include <iostream>
-double Quadratur_Mittepunkt() {
-    double a = 0.1;
-    double b = 0.1;
-    double epsilon = 0.1;
-    getExample(1, a, b, epsilon, false, 300);
-    std::cout << a << ' ' << b << ' ' << epsilon << '\n'; 
+#include <math.h>
+
+double Mittelpunktsregel(double a, double b) { return f((a + b) / 2); }
+
+double Adaptives_Zerlegen(double a, double b, double f_a, double f_b,
+                          double f_mpr, double epsilon, long local_depth,
+                          long *max_depth) {
+    if (fabs(f_mpr - (f_a + f_b) / 2) < epsilon) {
+        if (local_depth > *max_depth) 
+            *max_depth = local_depth;
+        return (f_a + f_b) / 6 + 4 * f_mpr / 6;
+    } else {
+        double mid = (a + b) / 2;
+        return Adaptives_Zerlegen(a, mid, f_a, f_mpr, Mittelpunktsregel(a, mid),
+                                  epsilon / 2, local_depth + 1, max_depth) / 2 +
+               Adaptives_Zerlegen(mid, b, f_mpr, f_b, Mittelpunktsregel(mid, b),
+                                  epsilon / 2, local_depth + 1, max_depth) / 2;
+    }
 }
 
-int main() {
-    Quadratur_Mittepunkt();
+void Quadratur() {
+    double a{};
+    double b{};
+    double epsilon{};
+    std::cout << "Id der Funktion dessen Integral approximiert werden soll: ";
+    int id{};
+    std::cin >> id;
+    getExample(id, a, b, epsilon, true, 300);
+    std::cout << a << " " << b << ' ' << ' ' << epsilon << '\n';
+    long max_depth{0};
+    double approx{Adaptives_Zerlegen(a, b, f(a), f(b), Mittelpunktsregel(a, b),
+                                     epsilon, 1, &max_depth)};
+    std::cout << "Bei Äquidistanter Schrittweite sind insgesamt "
+              << std::pow(2, max_depth) + 1
+              << " Funktionsauswertungen notwendig.\n";
+    checkSolution(approx);
 }
+
+int main() { Quadratur(); }
--- Code (3)/quadratur.h
+++ Code (3)/quadratur.h
@@ -1,4 +1,10 @@
 #ifndef _QUADRATUR_H
 #define _QUADRATUR_H
 
+double Mittelpunktsregel(double a, double b); 
+double Adaptives_Zerlegen(double a, double b, double f_a, double f_b,
+                          double f_mpr, double epsilon, long local_depth,
+                          long *max_depth);
+void Quadratur();
+ 
 #endif
